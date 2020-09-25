273f33b481a9bf14fe184c551e7e12c36b97d547
Eugene <eugene.kotsarev@gmail.com>
2020-09-26 00:33:32 +0300

    upd: Header, page layout

--- app/js/src/modules/header.js
+++ app/js/src/modules/header.js
@@ -1,49 +1,30 @@
 class Header {
-  constructor(nodeElement) {
-    this.nodeElement = nodeElement;
-    this.viewportHeight = null;
+  constructor(node) {
+    this.$node = node;
     this.main = document.querySelector('.page__container');
 
-    this.addEvents();
+    this.scrollEvents = this.scrollEvents.bind(this);
+
+    this.events();
   }
 
-  addEvents() {
-    onResize(this.updateCache.bind(this));
-    onScroll(this.onScroll.bind(this));
-    this.calcContentMargin();
-    onResize(this.calcContentMargin.bind(this));
+  events() {
+    onScroll(this.scrollEvents);
+  }
+
+  scrollEvents() {
+    getScrollPos() > window.innerHeight ? this.setAsFixed() : this.setAsNotFixed();
+
+    if (getScrollPos() < 300) this.$node.classList.remove('fixed-start');
   }
 
   setAsFixed() {
-    this.nodeElement.classList.add('fixed-start');
-    this.nodeElement.classList.add('fixed');
+    this.$node.classList.add('fixed-start');
+    this.$node.classList.add('fixed');
   }
 
   setAsNotFixed() {
-    this.nodeElement.classList.remove('fixed');
-  }
-
-  calcContentMargin() {
-    this.main.style.marginTop = `${this.nodeElement.getBoundingClientRect().height + 20}px`;
-  }
-
-  onScroll() {
-    const scrollTop = getScrollPos();
-    this.calcContentMargin();
-
-    if (scrollTop > window.innerHeight) {
-      this.setAsFixed();
-    } else {
-      this.setAsNotFixed();
-    }
-
-    if (scrollTop < 300) {
-      this.nodeElement.classList.remove('fixed-start');
-    }
-  }
-
-  updateCache() {
-    this.viewportHeight = window.innerHeight;
+    this.$node.classList.remove('fixed');
   }
 
   static init(elem) {
@@ -51,15 +32,9 @@ class Header {
   }
 }
 
-class HeaderUI {
-  static init() {
-    const header = document.querySelector('.js-header');
-    if (header) Header.init(header);
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  HeaderUI.init();
+  const header = document.querySelector('.js-header');
+  if (header) Header.init(header);
 });
 
-window.HeaderUI = HeaderUI;
+window.Header = Header;
